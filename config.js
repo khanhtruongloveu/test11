604ed33fa65af965df0249b7e2af5f8f24ea6310
khanhtruongloveu <88968349+khanhtruongloveu@users.noreply.github.com>
2021-08-20 00:29:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
+    introTitle: 'Hế nhô bro!🙋‍♂️🙋‍♂️🙋‍♂️',
     introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
-    btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
-    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
+    title: 'Cậu tớ đẹp trai hong😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là của tớ đó nha💖🖤💜💚💙💛🧡❤ ',
+    btnYes: 'Cóoo😍😘',
+    btnNo: 'Có ăn cứtttt💩💩💩',
+    question:'Nói cho tới cậu thấy tớ đẹp trai ở đâu đi😁',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    reply: 'Tại vì cậu đẹp trai, hài hước hihi🐱‍🏍 ',
+    mess: 'Tớ biết mà hehe 🥰. Cậu đã và mãi mãi trở thành một mảnh ghép của thanh xuân của tớ😉😊',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100034444426000' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
